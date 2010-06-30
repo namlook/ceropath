33e91dc97c5536da54a846c6a6468aa17efc81ca
Namlook <n.namlook@gmail.com>
2010-06-30 16:46:00 +0200

    remove some commented lines -- need some coffee

--- ceropath/websetup.py
+++ ceropath/websetup.py
@@ -24,43 +24,43 @@ def setup_app(command, conf, vars):
     db_name, host, port = conf.local_conf['db_name'], conf.local_conf['db_host'], conf.local_conf['db_port']
     con = Connection(host=host, port=int(port))
     con.register(register_models)
-#    con.drop_database(db_name)
-#    db = con[db_name]
-#
-#    pipeline_config = db.config.Pipeline()
-#    pipeline_config['programs'] = [
-#        {
-#            "path": u"/usr/bin",
-#            "name": u"sort",
-#            "input": u"STDIN",
-#            "options": u"-r",
-#            "output": None,
-#            "shell": False,
-#        },
-#        {
-#            "path": u"/usr/bin",
-#            "name": u"tr",
-#            "input": u"STDIN",
-#            "options": u"A-Z a-z",
-#            "output": None,
-#            "shell": False,
-#        }
-#    ]
-#    pipeline_config.save()
-#    documents_list = [
-#        'publication',
-#        'institute',
-#        'responsible',
-#        'organism_classification',
-#        'species_measurement',
-#        'site',
-#        'individual',
-#        'gene',
-#        'primer',
-#        'sequence',
-#        'rel_host_parasite',
-#    ]
-#
+    con.drop_database(db_name)
+    db = con[db_name]
+
+    pipeline_config = db.config.Pipeline()
+    pipeline_config['programs'] = [
+        {
+            "path": u"/usr/bin",
+            "name": u"sort",
+            "input": u"STDIN",
+            "options": u"-r",
+            "output": None,
+            "shell": False,
+        },
+        {
+            "path": u"/usr/bin",
+            "name": u"tr",
+            "input": u"STDIN",
+            "options": u"A-Z a-z",
+            "output": None,
+            "shell": False,
+        }
+    ]
+    pipeline_config.save()
+    documents_list = [
+        'publication',
+        'institute',
+        'responsible',
+        'organism_classification',
+        'species_measurement',
+        'site',
+        'individual',
+        'gene',
+        'primer',
+        'sequence',
+        'rel_host_parasite',
+    ]
+
     
     print "Convert csv to json. Please wait..."
     csv_path = os.path.join('data', 'csv')
