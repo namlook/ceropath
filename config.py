fd2d607b43c7e28ce4af0dfc0e809584cefc89fa
Namlook <n.namlook@gmail.com>
2010-07-23 14:12:47 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+
+username = ""
+password = ""
+
+google_map_api_key = ""
+
+json_allowed = [
+    'publication',
+    'institute',
+    'responsible',
+    'trait',
+    'organism_classification',
+    'species_measurement',
+    'site',
+    'individual',
+    'gene',
+    'primer',
+    'sequence',
+    'rel_host_parasite',
+]
+
