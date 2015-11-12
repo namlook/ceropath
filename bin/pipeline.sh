2511d84ceae75338c7ab31667df1957b30550b4a
namlook <nicolas.clairon@elkorado.com>
2015-11-12 12:27:19 +0100

    add programs and update pipeline to use linux binaries

--- bin/pipeline.sh
+++ bin/pipeline.sh
@@ -1,18 +1,25 @@
 
 
 BASENAME=$1
-CMD_PATH=/Users/namlook/Documents/projets/ceropath/bin
+BIN_PATH=/Users/namlook/Documents/projets/ceropath/bin
 
 # programs
-MUSCLE=$CMD_PATH/muscle3.8.31_i86darwin64
-DNADIST=dnadist
-DNADIST_CONFIG=$CMD_PATH/dnadist.cfg
-BIONJ=$CMD_PATH/BIONJ/BIONJ_MacOS
-# $PWD=`pwd`
-NWK2SVG=$CMD_PATH/nwk2svg.r
+
+## darwin
+# MUSCLE=$BIN_PATH/darwin/muscle3.8.31_i86darwin64
+# BIONJ=$BIN_PATH/darwin/BIONJ_MacOS
+# DNADIST=dnadist # installed via brew
+
+## linux
+MUSCLE=$BIN_PATH/muscle3.8.31_i86linux32
+BIONJ=$BIN_PATH/BIONJ_linux
+DNADIST=$BIN_PATH/dnadist
+
+DNADIST_CONFIG=$BIN_PATH/dnadist.cfg
+NWK2SVG=$BIN_PATH/nwk2svg.r
 
 # files
-COI_AFA=$CMD_PATH/files/coi_cbgp.afa
+COI_AFA=$BIN_PATH/files/coi_cbgp.afa
 
 $MUSCLE -in $BASENAME -out $BASENAME.afa &&
 $MUSCLE -profile -in1 $COI_AFA -in2 $BASENAME.afa -phyiout infile -maxiters 1 -diags &&
