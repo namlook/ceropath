0b1bbdb29abb29e136de1bab4a2b87404614b998
namlook <nicolas.clairon@elkorado.com>
2015-11-20 17:25:00 +0100

    use a precision of 0 for measurements

--- config/schemas/individual.js
+++ config/schemas/individual.js
@@ -117,156 +117,156 @@ module.exports = {
         // measurements
         headBodyMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'head & body (mm)',
             description: 'Head & Body (mm) (measurement_accuracy: 0)'
         },
         tailMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'tail (mm)',
             description: 'Tail (mm) (measurement_accuracy: 0)'
         },
         footMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'foot (mm)',
             description: 'Foot (mm) (measurement_accuracy: 0)'
         },
         earMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'ear (mm)',
             description: 'Ear (mm) (measurement_accuracy: 0)'
         },
         weighMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'weight (g)',
             description: 'Weight (g) (measurement_accuracy: 0)'
         },
         headMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'head (mm)',
             description: 'Head (mm) (measurement_accuracy: 0)'
         },
         breadthOfRostrumMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'breadth of rostrum (mm)',
             description: 'Breadth of Rostrum  (mm) (M01) (measurement_accuracy: 2)'
         },
         lengthOfRostrumMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'length of rostrum (mm)',
             description: 'Length of Rostrum (mm) (M02) (measurement_accuracy: 2)'
         },
         occipitoNasalLengthMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'occipitoNasal length (mm)',
             description: 'OccipitoNasal Length (mm) (M03) (measurement_accuracy: 2)'
         },
         interorbitalBreadthMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             description: 'Interorbital Breadth (mm) (M04) (measurement_accuracy: 2)'
         },
         breathOfBrainCaseMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'breath of brainCase (mm)',
             description: 'Breath of BrainCase (mm) (M05) (measurement_accuracy:)'
         },
         zygomaticBreadthMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'zygomatic breadth (mm)',
             description: 'Zygomatic Breadth (mm) (M06) (measurement_accuracy: 2)'
         },
         breadthOfIncisiveForaminaMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'breadth of incisive foramina (mm)',
             description: 'Breadth of Incisive Foramina (mm) (M07) (measurement_accuracy: 2)'
         },
         breadthOfFirstUpperMolarMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'breadth of first upper Molar (mm)',
             description: 'Breadth of first upper Molar (mm) (M08) (measurement_accuracy: 2)'
         },
         lengthOfDiastemaMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'length of Diastema (mm)',
             description: 'Length of Diastema (mm) (M09) (measurement_accuracy: 2)'
         },
         lenghtOfIncisiveForaminaMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'lenght of incisive foramina (mm)',
             description: 'Lenght of Incisive Foramina (mm) (M10) (measurement_accuracy: 2)'
         },
         lenghtOfBonyPalateMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'lenght of bony palate (mm)',
             description: 'Lenght of Bony Palate (mm) (M11) (measurement_accuracy: 2)'
         },
         postPalatalLengthMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'post-palatal length (mm)',
             description: 'PostPalatal Length (mm) (M12) (measurement_accuracy: 2)'
         },
         lengthOfAuditoryBullaMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'length of auditory bulla (mm)',
             description: 'Length of auditory Bulla (mm) (M13) (measurement_accuracy: 2)'
         },
         breadthOfMesopterygoidFossaMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'breadth of mesopterygoid fossa (mm)',
             description: 'Breadth of Mesopterygoid Fossa (mm) (M14) (measurement_accuracy: 2)'
         },
         breadthOfBonyPalateAtfirstMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'breadth of bony palate at first (mm)',
             description: 'Breadth of Bony Palate at first  (mm) (M15) (measurement_accuracy: 2)'
         },
         crownLengthOfMaxillaryMolarMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'crown length of maxillary molar (mm)',
             description: 'Crown Length of Maxillary Molar  (mm) (M16) (measurement_accuracy: 2)'
         },
         breathOfZygomaticPlateMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'breath of zygomatic plate (mm)',
             description: 'Breath of Zygomatic Plate (mm) (M17) (measurement_accuracy: 2)'
         },
         heightOfBraincaseMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'height of braincase (mm)',
             description: 'Height of Braincase (mm) (M18) (measurement_accuracy: 2)'
         },
         spleenWeightMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'spleen weight (mm)',
             description: 'Spleen weight (mg) (measurement_accuracy: 0)'
         },
         agpdMeasurement: {
             type: 'number',
-            validate: [{precision: 4}],
+            validate: [{precision: 0}],
             label: 'AGPD (mm)',
             description: 'AGPD (mm) (measurement_accuracy: 0)'
         },
